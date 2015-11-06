7dbc985247fbdf887bf5ae5d7379179463f560b3
weichi <weichi@egoonet.com>
2015-11-06 15:49:15 +0800

    代码微调：将index.html代码进行格式化，并将move.js精简重命名为common.js

---
+++ public/common.js
@@ -0,0 +1,92 @@
+//判断字符串是否为空
+function IsStringEmpty(str)
+{
+    if(str && str!='')
+        return false;
+    else
+        return true;
+}
+
+//获取页面的参数
+function request(paras) {
+    var url = location.href;
+    var paraString = url.substring(url.indexOf("#") + 1, url.length).split("&");
+    var paraObj = {}
+    for (i = 0; j = paraString[i]; i++) {
+        paraObj[j.substring(0, j.indexOf("=")).toLowerCase()] = j.substring(j.indexOf("=") + 1, j.length);
+    }
+    var returnValue = paraObj[paras.toLowerCase()];
+    if (typeof(returnValue) == "undefined") {
+        return "";
+    } else {
+        return returnValue;
+    }
+}
+
+//判断参数
+function getParams(paras) {
+    if (paras == "" || paras == undefined || paras == null) {
+        paras = false;
+    } else if (paras == 'false') {
+        paras = false;
+    } else if (paras == 'true') {
+        paras = true;
+    } else {
+        paras = false;
+    }
+    return paras;
+}
+
+function Map() {
+
+    var struct = function (key, value) {
+        this.key = key;
+        this.value = value;
+    };
+
+    var put = function (key, value) {
+        for (var i = 0; i < this.arr.length; i++) {
+            if (this.arr[i].key === key) {
+                this.arr[i].value = value;
+                return;
+            }
+        }
+        this.arr[this.arr.length] = new struct(key, value);
+    };
+
+
+    var get = function (key) {
+        for (var i = 0; i < this.arr.length; i++) {
+            if (this.arr[i].key === key) {
+                return this.arr[i].value;
+            }
+        }
+        return null;
+    };
+
+    var remove = function (key) {
+        var v;
+        for (var i = 0; i < this.arr.length; i++) {
+            v = this.arr.pop();
+            if (v.key === key) {
+                continue;
+            }
+            this.arr.unshift(v);
+        }
+    };
+
+    var size = function () {
+        return this.arr.length;
+    };
+
+    var isEmpty = function () {
+        return this.arr.length <= 0;
+    };
+
+    this.arr = new Array();
+    this.get = get;
+    this.put = put;
+    this.remove = remove;
+    this.size = size;
+    this.isEmpty = isEmpty;
+}
